ca838840289d5ee4527efaa492fcd53b9b479438
Ale Sanchez <alesanchezmedina@gmail.com>
2021-07-25 21:13:52 +0200

    fix: Added log file path to init_logger

--- covid_data/__init__.py
+++ covid_data/__init__.py
@@ -7,7 +7,7 @@ from .logger import init_logger
 
 load_dotenv()
 
-init_logger()
+init_logger(os.path.join(os.path.dirname(__file__), "../logs/covid_data.log"))
 
 with open(os.path.join(os.path.dirname(__file__), "../", "pyproject.toml"), "r") as f:
     project_info = f.read()
