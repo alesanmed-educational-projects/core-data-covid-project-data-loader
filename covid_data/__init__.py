0ec742682b0194c2c2cd4ee997b96f5c25404230
Ale Sánchez <alesanchez@us.es>
2021-07-26 20:52:15 +0200

    fix: Moved dotenv loading to app file to avoid setup errors

--- covid_data/__init__.py
+++ covid_data/__init__.py
@@ -1,14 +1,6 @@
 import os
 import re
 
-from dotenv import load_dotenv
-
-from .logger import init_logger
-
-load_dotenv()
-
-init_logger(os.path.join(os.path.dirname(__file__), "../logs/covid_data.log"))
-
 with open(os.path.join(os.path.dirname(__file__), "../", "pyproject.toml"), "r") as f:
     project_info = f.read()
 
