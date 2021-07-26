638c058e384d235790676aa212c0248603f3c215
Ale Sánchez <alesanchez@us.es>
2021-07-26 20:55:48 +0200

    feat: Added tests to query

--- covid_data/db/test_queries.py
+++ covid_data/db/test_queries.py
@@ -2,16 +2,42 @@ import unittest
 from unittest import TestCase
 from unittest.mock import MagicMock
 
+from sqlalchemy.engine.base import Connection
+
 import covid_data.db as db
 from covid_data.db.queries import country_exists
 from covid_data.test_utils import MockDB
 
 
-class TestQueries(TestCase):
-    def test_country_exists(self):
+class TestCountryExists(TestCase):
+    def test_execute_called(self):
+        """Country exists should call execute once"""
+
+        Connection.execute = MagicMock(return_value=[(0,)])
+
+        country_exists("Test", db.get_db())
+
+        Connection.execute.assert_called_once()
+
+    def test_should_return_false(self):
+        """Country exists should return false"""
         mockDb = MockDB()
 
-        country_exists("Test", mockDb)
+        mockDb.execute_return = [(0,)]
+
+        res = country_exists("Test", mockDb)
+
+        self.assertFalse(res)
+
+    def test_should_return_true(self):
+        """Country exists should return true"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = [(1,)]
+
+        res = country_exists("Test", mockDb)
+
+        self.assertTrue(res)
 
 
 if __name__ == "__main__":
--- covid_data/test_utils/__init__.py
+++ covid_data/test_utils/__init__.py
@@ -1,4 +1,4 @@
-class MockDB:
+class MockDB(object):
     execute_return = None
 
     def connect(self, *args, **kwargs):
@@ -6,3 +6,9 @@ class MockDB:
 
     def execute(self, *args, **kwargs):
         return self.execute_return
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        pass
