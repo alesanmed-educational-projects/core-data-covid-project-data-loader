ecbb0c486e3d001ebba62aa8c4d967ebcd281fe7
Ale Sánchez <alesanchez@us.es>
2021-07-28 10:35:21 +0200

    feat: Solved linting problems

--- covid_data/db/queries.py
+++ covid_data/db/queries.py
@@ -1,16 +1,110 @@
-from sqlalchemy.engine.base import Connection, Engine
+from sqlalchemy.engine import Connection
+from sqlalchemy.engine.base import Engine
 
 
-def country_exists(country: str, engine: Engine) -> bool:
+def place_exists(place: str, engine: Engine) -> str:
     with engine.connect() as conn:
         conn: Connection
 
-        result = conn.execute(
-            "SELECT COUNT(*) FROM countries WHERE name = ':name'", {"name": country}
+        result = list(
+            conn.exec_driver_sql(
+                "SELECT id FROM countries WHERE name = %(name)s", {"name": place}
+            )
         )
 
-        return list(result)[0][0] > 0
+        if not len(result):
+            return None
+        else:
+            return result[0][0]
+
+
+def create_country(country: dict, engine: Engine) -> str:
+    with engine.connect() as conn:
+        conn: Connection
+
+        result = conn.exec_driver_sql(
+            (
+                "INSERT INTO countries "
+                "VALUES ("
+                "DEFAULT, "
+                "%(name)s, "
+                "%(alpha2)s, "
+                "%(alpha3)s, "
+                "ST_SetSRID(ST_MakePoint(%(lng)s, %(lat)s), 4326)"
+                ") "
+                "RETURNING id"
+            ),
+            country,
+        )
+
+        return list(result)[0][0]
+
+
+def create_province(province: dict, engine: Engine) -> str:
+    with engine.connect() as conn:
+        conn: Connection
+
+        result = conn.exec_driver_sql(
+            (
+                "INSERT INTO provinces VALUES ("
+                "DEFAULT, "
+                "%(name)s, "
+                "ST_SetSRID(ST_MakePoint(%(lng)s, %(lat)s), 4326), "
+                "%(code)s, "
+                "%(country_id)s"
+                ") RETURNING id"
+            ),
+            province,
+        )
+
+        return list(result)[0][0]
+
+
+def create_county(county: dict, engine: Engine) -> str:
+    with engine.connect() as conn:
+        conn: Connection
+
+        result = conn.exec_driver_sql(
+            (
+                "INSERT INTO provinces VALUES ("
+                "DEFAULT, "
+                "%(name)s, "
+                "ST_SetSRID(ST_MakePoint(%(lng)s, %(lat)s), 4326), "
+                "%(code)s, "
+                "%(province_id)s"
+                ") RETURNING id"
+            ),
+            county,
+        )
+
+        return list(result)[0][0]
+
+
+def create_case(case: dict, engine: Engine) -> bool:
+    with engine.connect() as conn:
+        conn: Connection
+
+        conn.exec_driver_sql(
+            (
+                "INSERT INTO cases VALUES (DEFAULT, %(type)s, %(amount)s, %(date)s, %(country_id)s, "
+                "%(province_id)s, %(county_id)s) "
+                "ON CONFLICT ("
+                "type, "
+                "date, "
+                "country_id, "
+                "type, "
+                "date, "
+                "country_id, "
+                "COALESCE(province_id, -1), COALESCE(county_id, -1)"
+                ") DO UPDATE SET amount=%(amount)s"
+            ),
+            case,
+        )
+
+        return True
 
 
 if __name__ == "__main__":
-    country_exists("Spain")
+    from covid_data.db import get_db
+
+    print(place_exists("Spain", get_db()))
