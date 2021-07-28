dfca83fcc918760c9f782a600702aaf1e9ff9e42
Ale Sánchez <alesanchez@us.es>
2021-07-28 10:35:54 +0200

    feat: Loading process almost finished. Some tests remaining

--- covid_data/commands/load_csv.py
+++ covid_data/commands/load_csv.py
@@ -1,45 +1,241 @@
 import os
+from dataclasses import dataclass
+from datetime import datetime
 from logging import getLogger
-from typing import NamedTuple
 
 import pandas as pd
 import requests
+from sqlalchemy.engine.base import Engine
 
-from covid_data.db.queries import country_exists
+from covid_data.db import get_db, queries
+from covid_data.db.queries import create_case, place_exists
 
 URL = f"https://api.opencagedata.com/geocode/v1/json?key={os.environ.get('CAGEDATA_API_KEY')}&no_annotations=1"
-logger = getLogger('covid_data')
+logger = getLogger("covid_data")
 
-class Point(NamedTuple):
-    lat: float
-    lng: float
 
-class PlaceInfo(NamedTuple):
-    alpha2Code: str
-    alpha3Code: str
+class PlaceInfoFetchException(Exception):
+    pass
+
+
+class PlaceType:
+    COUNTRY: str = "country"
+    STATE: str = "state"
+    CITY: str = "city"
+    TERRITORY: str = "territory"
+
+
+class CaseType:
+    CONFIRMED: str = "confirmed"
+    DEAD: str = "dead"
+    RECOVERED: str = "recovered"
+
+
+@dataclass
+class Point:
+    lat: float = None
+    lng: float = None
+
+
+@dataclass
+class PlaceInfo:
+    alpha2: str
+    alpha3: str
+    category: str
     type: str
-    sub_code: str
-    location: Point
+    continent: str
+    country: str
+    country_code: str
+    location: Point = None
+    city: str = None
+    county: str = None
+    county_code: str = None
+    political_union: str = None
+    state: str = None
+    state_code: str = None
 
 
 def get_place_info(place: str) -> PlaceInfo:
-    response = requests.get(URL, {
-        "q": place
-    })
+    response = requests.get(URL, {"q": place})
 
     if response.status_code > 399:
-        logger.error(f"Error while retrieving place info {place}. Error {response.status_code}")
+        logger.error(
+            f"Error while retrieving place info {place}. Error {response.status_code}"
+        )
         logger.error(response.json())
 
         return None
-    
+
     response_body = response.json()
 
-def insert_data(df: pd.DataFrame) -> bool:
-    for index, row in df.iterrows():
-        state = row["Province/State"]
-        country = row["Province/State"]
-        latlng = (row["Lat"], row["Long"])
+    place = response_body["results"][0]
 
-        if not country_exists(country):
-            pass
+    components: dict = place["components"]
+
+    components["alpha2"] = components.pop("ISO_3166-1_alpha-2")
+    components["alpha3"] = components.pop("ISO_3166-1_alpha-3")
+    components["type"] = components.pop("_type")
+    components["category"] = components.pop("_category")
+
+    if components["type"] == PlaceType.TERRITORY:
+        components["type"] = PlaceType.STATE
+        components["state"] = components.pop("territory")
+
+    res: PlaceInfo
+
+    res = PlaceInfo(**components)
+
+    res.location = Point(**place["geometry"])
+
+    return res
+
+
+def extract_location(place_info: PlaceInfo, place_type: str) -> Point:
+    res = Point()
+    if place_info.type != place_type:
+        correct_place_info = get_place_info(
+            place_info[f"{place_type.replace(PlaceType.COUNTRY, 'county')}_code"]
+        )
+        if correct_place_info is None:
+            raise PlaceInfoFetchException()
+
+        res.lat = correct_place_info.location.lat
+        res.lng = correct_place_info.location.lng
+    else:
+        res.lat = place_info.location.lat
+        res.lng = place_info.location.lng
+
+    return res
+
+
+def create_country(country: str, engine: Engine, place_info: PlaceInfo = None) -> str:
+    if country_id := place_exists(country, engine):
+        return country_id
+
+    if not place_info:
+        place_info = get_place_info(country)
+        if place_info is None:
+            raise PlaceInfoFetchException()
+
+    point = extract_location(place_info, PlaceType.COUNTRY)
+
+    country_data = {
+        "name": place_info.country,
+        "alpha2": place_info.alpha2,
+        "alpha3": place_info.alpha3,
+        "lat": point.lat,
+        "lng": point.lng,
+    }
+
+    return queries.create_country(country_data, engine)
+
+
+def create_province(province: str, engine: Engine, place_info: PlaceInfo = None) -> str:
+    if province_id := place_exists(province, engine):
+        return province_id
+
+    if not place_info:
+        place_info = get_place_info(province)
+        if place_info is None:
+            raise PlaceInfoFetchException()
+
+    country_id = create_country(place_info.country, engine, place_info)
+
+    point = extract_location(place_info, PlaceType.COUNTRY)
+
+    province_data = {
+        "name": place_info.state,
+        "code": place_info.state_code,
+        "country_id": country_id,
+        "lat": point.lat,
+        "lng": point.lng,
+    }
+
+    return queries.create_province(province_data, engine)
+
+
+def create_county(county: str, engine: Engine, place_info: PlaceInfo = None) -> str:
+    if county_id := place_exists(county, engine):
+        return county_id
+
+    if not place_info:
+        place_info = get_place_info(county)
+        if place_info is None:
+            raise PlaceInfoFetchException()
+
+    province_id = create_province(place_info, engine)
+
+    point = extract_location(place_info, PlaceType.CITY)
+
+    county_data = {
+        "name": place_info.county,
+        "code": place_info.county_code or "",
+        "lat": point.lat,
+        "lng": point.lng,
+        "province_id": province_id,
+    }
+
+    return queries.create_county(county_data, engine)
+
+
+def insert_data(df: pd.DataFrame, case_type: CaseType) -> bool:
+    engine = get_db()
+
+    num_rows = df.shape[0]
+
+    for index, row in df.iterrows():
+        print(f"Processing row {index + 1}/{num_rows}")
+        state = row["Province/State"]
+        country = row["Country/Region"]
+        province_id = None
+        country_id = None
+
+        try:
+            if not pd.isna(state):
+                province_id = create_province(state, engine)
+
+            if not pd.isna(country):
+                country_id = create_country(country, engine)
+        except PlaceInfoFetchException:
+            logger.error(f"Skipping line {index}")
+
+        row = row.drop(["Province/State", "Country/Region", "Lat", "Long"])
+
+        num_columns = row.shape[0]
+
+        for i, date_str in enumerate(row.index):
+            print(f"Processing case {i+1}/{num_columns}", end="\r")
+            date_padded: str
+
+            date_padded = "/".join([part.zfill(2) for part in date_str.split("/")])
+
+            date = datetime.strptime(date_padded, "%m/%d/%y")
+
+            case = {
+                "type": case_type,
+                "amount": row[date_str],
+                "date": date,
+                "country_id": country_id,
+                "province_id": province_id,
+                "county_id": None,
+            }
+
+            create_case(case, engine)
+
+        print()
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    from covid_data.logger import init_logger
+
+    load_dotenv()
+
+    init_logger(os.path.join(os.path.dirname(__file__), "../../logs/covid_data.log"))
+
+    df = pd.read_csv(
+        os.path.join(os.path.dirname(__file__), "../../data/confirmed_global.csv")
+    )
+
+    insert_data(df, CaseType.CONFIRMED)
--- covid_data/db/test_queries.py
+++ covid_data/db/test_queries.py
@@ -1,43 +1,202 @@
 import unittest
+from collections import namedtuple
 from unittest import TestCase
 from unittest.mock import MagicMock
 
-from sqlalchemy.engine.base import Connection
-
-import covid_data.db as db
-from covid_data.db.queries import country_exists
+from covid_data.db.queries import (
+    create_country,
+    create_county,
+    create_province,
+    place_exists,
+)
 from covid_data.test_utils import MockDB
 
 
-class TestCountryExists(TestCase):
-    def test_execute_called(self):
-        """Country exists should call execute once"""
+class TestPlaceExists(TestCase):
+    mock_id = "TestID"
 
-        Connection.execute = MagicMock(return_value=[(0,)])
-
-        country_exists("Test", db.get_db())
-
-        Connection.execute.assert_called_once()
-
-    def test_should_return_false(self):
-        """Country exists should return false"""
+    def test_connect_called(self):
+        """Place exists should call connect once"""
         mockDb = MockDB()
 
         mockDb.execute_return = [(0,)]
 
-        res = country_exists("Test", mockDb)
+        mockDb.connect = MagicMock(return_value=mockDb)
 
-        self.assertFalse(res)
+        place_exists("Test", mockDb)
 
-    def test_should_return_true(self):
-        """Country exists should return true"""
+        mockDb.connect.assert_called_once()
+
+    def test_execute_called(self):
+        """Place exists should call execute once"""
         mockDb = MockDB()
 
-        mockDb.execute_return = [(1,)]
+        mockDb.execute = MagicMock(return_value=[(0,)])
 
-        res = country_exists("Test", mockDb)
+        place_exists("Test", mockDb)
 
-        self.assertTrue(res)
+        mockDb.execute.assert_called_once()
+
+    def test_should_return_id(self):
+        """Place exists should return id if exists"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = [self.mock_id]
+
+        res = place_exists("Test", mockDb)
+
+        self.assertEqual(self.mock_id, res)
+
+    def test_should_return_none(self):
+        """Place exists should return none if empty result"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = []
+
+        res = place_exists("Test", mockDb)
+
+        self.assertIsNone(res)
+
+
+class TestCreateCountry(TestCase):
+    InsertedPrimaryKey = namedtuple("InsertedPrimaryKey", "id")
+    InsertResult = namedtuple("InsertResult", "inserted_primary_key")
+
+    mock_id = "TestID"
+
+    execute_return = InsertResult(InsertedPrimaryKey(mock_id))
+
+    mock_country = {
+        "name": "Test Country",
+        "lat": 20.1234,
+        "lng": 120.1234,
+    }
+
+    def test_connect_called(self):
+        """Create country should call connect once"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = self.execute_return
+
+        mockDb.connect = MagicMock(return_value=mockDb)
+
+        create_country(self.mock_country, mockDb)
+
+        mockDb.connect.assert_called_once()
+
+    def test_execute_called(self):
+        """Create country should call execute once"""
+        mockDb = MockDB()
+
+        mockDb.execute = MagicMock(return_value=self.execute_return)
+
+        create_country(self.mock_country, mockDb)
+
+        mockDb.execute.assert_called_once()
+
+    def test_should_return_inserted_id(self):
+        """Create country should return inserted id"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = self.execute_return
+
+        res = create_country(self.mock_country, mockDb)
+
+        self.assertEqual(res, self.mock_id)
+
+
+class TestCreateProvince(TestCase):
+    InsertedPrimaryKey = namedtuple("InsertedPrimaryKey", "id")
+    InsertResult = namedtuple("InsertResult", "inserted_primary_key")
+
+    mock_id = "TestID"
+
+    execute_return = InsertResult(InsertedPrimaryKey(mock_id))
+
+    mock_province = {
+        "name": "Test Province",
+        "lat": 20.1234,
+        "lng": 120.1234,
+    }
+
+    def test_connect_called(self):
+        """Create province should call connect once"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = self.execute_return
+
+        mockDb.connect = MagicMock(return_value=mockDb)
+
+        create_province(self.mock_province, mockDb)
+
+        mockDb.connect.assert_called_once()
+
+    def test_execute_called(self):
+        """Create province should call execute once"""
+        mockDb = MockDB()
+
+        mockDb.execute = MagicMock(return_value=self.execute_return)
+
+        create_province(self.mock_province, mockDb)
+
+        mockDb.execute.assert_called_once()
+
+    def test_should_return_inserted_id(self):
+        """Create province should return inserted id"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = self.execute_return
+
+        res = create_province(self.mock_province, mockDb)
+
+        self.assertEqual(res, self.mock_id)
+
+
+class TestCreateCounty(TestCase):
+    InsertedPrimaryKey = namedtuple("InsertedPrimaryKey", "id")
+    InsertResult = namedtuple("InsertResult", "inserted_primary_key")
+
+    mock_id = "TestID"
+
+    execute_return = InsertResult(InsertedPrimaryKey(mock_id))
+
+    mock_county = {
+        "name": "Test County",
+        "lat": 20.1234,
+        "lng": 120.1234,
+    }
+
+    def test_connect_called(self):
+        """Create county should call connect once"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = self.execute_return
+
+        mockDb.connect = MagicMock(return_value=mockDb)
+
+        create_county(self.mock_county, mockDb)
+
+        mockDb.connect.assert_called_once()
+
+    def test_execute_called(self):
+        """Create county should call execute once"""
+        mockDb = MockDB()
+
+        mockDb.execute = MagicMock(return_value=self.execute_return)
+
+        create_county(self.mock_county, mockDb)
+
+        mockDb.execute.assert_called_once()
+
+    def test_should_return_inserted_id(self):
+        """Create county should return inserted id"""
+        mockDb = MockDB()
+
+        mockDb.execute_return = self.execute_return
+
+        res = create_county(self.mock_county, mockDb)
+
+        self.assertEqual(res, self.mock_id)
 
 
 if __name__ == "__main__":
