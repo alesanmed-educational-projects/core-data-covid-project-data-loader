d6618b4f72db0bbafdd8fb127cc1af23e92a56b1
Ale Sánchez <alesanchez@us.es>
2021-07-29 20:06:26 +0200

    feat: Logged uncaught exceptions

--- covid_data/logger.py
+++ covid_data/logger.py
@@ -1,14 +1,15 @@
 # encoding: utf-8
 import logging
+import sys
 
 
-def init_logger(logfile: str):
+def init_logger(logfile: str, level: int = logging.DEBUG):
     """Initialize the root logger and standard log handlers."""
     log_formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
     root_logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
+    root_logger.setLevel(level)
 
     file_handler = logging.FileHandler(logfile)
     file_handler.setFormatter(log_formatter)
@@ -17,3 +18,10 @@ def init_logger(logfile: str):
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_formatter)
     root_logger.addHandler(console_handler)
+
+    sys.excepthook = log_uncaught_exception
+
+
+def log_uncaught_exception(type, value, traceback):
+    root_logger = logging.getLogger()
+    root_logger.exception(f"Uncaught exception: {value}")
